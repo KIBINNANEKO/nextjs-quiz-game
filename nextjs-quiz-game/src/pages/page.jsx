edd027616ad5187979ab9df700b81fd66d08cc04
Kostiantyn Ivanov <kostia10kot@gmail.com>
2023-08-04 23:59:31 +0300

    impl: add page

--- nextjs-quiz-game/src/pages/page.jsx
+++ nextjs-quiz-game/src/pages/page.jsx
@@ -1,8 +1,11 @@
+import Layout from "@/components/layout"
 
 export default function Home() {
   return (
-    <main>
- 
-    </main>
+    <Layout>
+      <main>
+  
+      </main>
+    </Layout>
   )
 }

b95e7f77cb0b7d5b99193b7f01c5ada0ae3fb05d
Kostiantyn Ivanov <kostia10kot@gmail.com>
2023-08-16 14:39:07 +0300

    impl: add options for agents services

--- nextjs-quiz-game/src/services/agents.js
+++ nextjs-quiz-game/src/services/agents.js
@@ -1,12 +1,19 @@
-export default fetchAllAgents = async () => {
-	try{
-
-		const response = await fetch(FETCH_AGENTS);
+export const fetchAllAgents = async (language) => {
+	try {
+		const response = await fetch(process.env.FETCH_AGENTS + '?lang=' + language);
 		const agents = await response.json();
 		return agents;
-
-	} catch (error){
-		console.log('Fetch agents error: ' + error)
+	} catch (error) {
+		console.log('Fetch agents error: ' + error);
 	}
+};
 
+export const fetchAgentById = async (id, language) => {
+	try {
+		const response = await fetch(process.env.FETCH_AGENTS + '/' + id + '?lang=' + language);
+		const agent = await response.json();
+		return agent;
+	} catch (error) {
+		console.log('Fetch agent by id error: ' + error);
+	}
 };

14067d931d9c28663bb39fe6558e651f86b7eb03
Kostiantyn Ivanov <kostia10kot@gmail.com>
2023-08-27 19:46:55 +0300

    impl: add styles names on agent page

--- nextjs-quiz-game/src/app/agents/[...params]/page.jsx
+++ nextjs-quiz-game/src/app/agents/[...params]/page.jsx
@@ -31,39 +31,39 @@ export default async function Page({ params }) {
 
 	
 	return (
-		<div className={styles.}>
-			<div className={styles.}>
-				<div className={styles.}>
-					<div className={styles.}>
-						<Image src={displayIcon} alt={displayName} width={120} height={120}/>
+		<div className={styles.page}>
+			<div className={styles.block_1}>
+				<div className={styles.present_agent}>
+					<div>
+						<Image src={displayIcon} alt={displayName} width={120} height={120} className={styles.agent_icon} />
 					</div>
-					<div className={styles.}><h1>{displayName}</h1></div>
+					<div className={styles.agent_name}><h1>{displayName}</h1></div>
 				</div>
-				<div className={styles.}>
-					<div className={styles.}>
+				<div className={styles.present_role}>
+					<div className={styles.role_icon}>
 						<Image src={displayIconRole} alt={displayNameRole} width={35} height={35} />
 					</div>
-					<div className={styles.}><h3>Role: {displayNameRole}</h3></div>
+					<div className={styles.role_name}><h3>Role: {displayNameRole}</h3></div>
 				</div>
-				<div className={styles.}>{description}</div>
-				<div className={styles.}>
-					<h4 className={styles.}>SPECIAL ABILITIES</h4>
-					<div className={styles.}>
+				<div className={styles.description}>{description}</div>
+				<div className={styles.abilities_block}>
+					<h4 className={styles.abilities_title}>SPECIAL ABILITIES</h4>
+					<div className={styles.abilities_items}>
 						{abilities.map(item => (
-							<div className={styles.}>
-								<div className={styles.}>
+							<div className={styles.ability_item}>
+								<div className={styles.ability_icon}>
 									<Image src={item.displayIcon} alt={item.displayName} width={100} height={100} />
 								</div>
-								<div className={styles.}>
-									<h5 className={styles.}>{item.displayName}</h5>
-									<p className={styles.}>{item.description}</p>
+								<div className={styles.abilities_text}>
+									<h5 className={styles.ability_name}>{item.displayName}</h5>
+									<p className={styles.ability_description}>{item.description}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</div>
 			</div>
-			<div>
+			<div className={styles.block_2}> 
 				
 			</div>
 		</div>
